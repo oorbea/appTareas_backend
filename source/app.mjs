4d0339f2df14b04f84fc2e9c99b4f7dcff1a6144
oorbea <meck.porky@gmail.com>
2025-01-24 16:33:14 +0100

    API base structure

--- source/app.mjs
+++ source/app.mjs
@@ -2,9 +2,20 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
-dotenv.config();
+dotenv.config({ path: '../.env' });
 
 const app = express();
 app.disable('x-powered-by');
 app.use(cors());
 app.use(express.json());
+
+const PORT = process.env.PORT ?? 5000;
+const BASE_URL = `http://localhost:${PORT}`;
+
+app.use((req, res) => {
+  res.status(404).send('<h1>404 - Página no encontrada</h1>');
+});
+
+app.listen(PORT, () => {
+  console.log('Server is running on ' + BASE_URL);
+});
