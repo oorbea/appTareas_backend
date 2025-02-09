1deaaf89a15972ba2fa552ee5b04f3fde5d15feb
oorbea <meck.porky@gmail.com>
2025-02-09 19:42:30 +0100

    task routes structure

--- source/routes/index.mjs
+++ source/routes/index.mjs
@@ -1,12 +1,14 @@
 import express from 'express';
 import userRoutes from './userRoutes.mjs';
 import taskListRoutes from './taskListRoutes.mjs';
+import taskRoutes from './taskRoutes.mjs';
 import adminRoutes from './adminRoutes.mjs';
 
 const router = express.Router();
 
 router.use('/user', userRoutes);
 router.use('/task_list', taskListRoutes);
+router.use('/task', taskRoutes);
 router.use('/admin', adminRoutes);
 
 export default router;
---
+++ source/routes/taskRoutes.mjs
@@ -0,0 +1,6 @@
+import express from 'express';
+import { authenticate } from '../authenticate.mjs';
+
+const router = express.Router();
+
+export default router;
