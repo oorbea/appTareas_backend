60f1785cb046400d96ace0fcd554a1bb601c9140
oorbea <meck.porky@gmail.com>
2025-02-17 01:14:15 +0100

    Token authentication protected

--- source/authenticate.ts
+++ source/authenticate.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import User from './models/user';
 
 dotenv.config();
 
@@ -24,21 +25,32 @@ const authenticate = (req: Request, res: Response, next: NextFunction) => {
 
   const token = authHeader.split(' ')[1];
 
+  let decoded;
   try {
-    const decoded = jwt.verify(token, JWT_SECRET) as UserPayload;
+    decoded = jwt.verify(token, JWT_SECRET) as UserPayload | null | undefined;
+  } catch {
+    res.status(403).json({ error: 'Token no válido o caducado' });
+  }
 
-    if (!decoded) {
-      res.status(403).json({ error: 'Token no válido o caducado' });
+  if (!decoded) {
+    res.status(403).json({ error: 'Token no válido o caducado' });
+    return;
+  }
+
+  const { id } = decoded;
+  User.findByPk(id).then((user) => {
+    if (!user || !user.enabled) {
+      res.status(404).json({ error: 'Usuario no encontrado' });
       return;
     }
 
     req.user = decoded;
-
     next();
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Error inesperado al autenticar usuario' });
-  }
+  })
+    .catch(error => {
+      console.error(error);
+      res.status(500).json({ error: 'Error inesperado al autenticar usuario' });
+    });
 };
 
 export default authenticate;
